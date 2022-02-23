c5e442fef8feb4af44f3b11083cac3bdab21d3d2
kgeddis-ch <kgeddis@companieshouse.gov.uk>
2022-02-23 15:57:35 +0000

    Unit test multiple insolvency cases

--- src/test/java/uk/gov/companieshouse/insolvency/delta/mapper/InsolvencyMapperTest.java
+++ src/test/java/uk/gov/companieshouse/insolvency/delta/mapper/InsolvencyMapperTest.java
@@ -27,7 +27,7 @@ class InsolvencyMapperTest {
     public void setUp() throws IOException {
         mapper = new ObjectMapper();
 
-        String path = "insolvency-delta-example.json";
+        String path = "insolvency-delta-complex.json";
         String input =
                 FileCopyUtils.copyToString(new InputStreamReader(ClassLoader.getSystemClassLoader().getResourceAsStream(path)));
 
@@ -44,37 +44,80 @@ class InsolvencyMapperTest {
         assertThat(insolvency).isNotNull();
         assertThat(insolvencyTarget).isNotNull();
         assertThat(insolvencyTarget.getDeltaAt()).isEqualTo("20211008152823383176");
+        assertThat(insolvencyTarget.getCases().size()).isEqualTo(2);
 
-        assertThat(insolvencyTarget.getCases().size()).isEqualTo(1);
+        // FIRST CASE
         ModelCase firstCase = insolvencyTarget.getCases().get(0);
         assertThat(firstCase.getNumber()).isEqualTo(1);
-        assertThat(firstCase.getType()).isEqualTo(ModelCase.TypeEnum.MEMBERS_VOLUNTARY_LIQUIDATION);
-        assertThat(firstCase.getPractitioners().size()).isEqualTo(1);
+        assertThat(firstCase.getType()).isEqualTo(ModelCase.TypeEnum.ADMINISTRATIVE_RECEIVER);
+        assertThat(firstCase.getPractitioners().size()).isEqualTo(2);
 
-
+        // FIRST PRACTITIONER
         Practitioners firstPractitioner = firstCase.getPractitioners().get(0);
-        assertThat(firstPractitioner.getName()).isEqualTo("Bernard Hoffman");
-        assertThat(firstPractitioner.getAppointedOn()).isEqualTo(LocalDate.of(2020, 05, 06));
+        assertThat(firstPractitioner.getName()).isEqualTo("Michael Horrocks");
+        assertThat(firstPractitioner.getAppointedOn()).isEqualTo(LocalDate.of(2004, 11, 22));
+        assertThat(firstPractitioner.getCeasedToActOn()).isEqualTo(LocalDate.of(2006, 5, 5));
         assertThat(firstPractitioner.getRole()).isEqualTo(Practitioners.RoleEnum.PRACTITIONER);
 
-        PractitionerAddress address = firstPractitioner.getAddress();
-        assertThat(address.getAddressLine1()).isEqualTo("Yerrill Murphy Edelman House");
-        assertThat(address.getAddressLine2()).isEqualTo("1238 High Road");
-        assertThat(address.getLocality()).isEqualTo("Whetstone");
-        assertThat(address.getRegion()).isEqualTo("London");
-        assertThat(address.getCountry()).isNull();
-        assertThat(address.getPostalCode()).isEqualTo("N20 0LH");
+        PractitionerAddress firstAddress = firstPractitioner.getAddress();
+        assertThat(firstAddress.getAddressLine1()).isEqualTo("Free From Gluten");
+        assertThat(firstAddress.getAddressLine2()).isEqualTo("101 Barbirolli Square");
+        assertThat(firstAddress.getLocality()).isEqualTo("Lower Mosley Street");
+        assertThat(firstAddress.getRegion()).isEqualTo("Manchester");
+        assertThat(firstAddress.getCountry()).isNull();
+        assertThat(firstAddress.getPostalCode()).isEqualTo("M2 3PW");
 
-        assertThat(firstCase.getDates().size()).isEqualTo(2);
+        // SECOND PRACTITIONER
+        Practitioners secondPractitioner = firstCase.getPractitioners().get(1);
+        assertThat(secondPractitioner.getName()).isEqualTo("Craig Livesey");
+        assertThat(secondPractitioner.getAppointedOn()).isEqualTo(LocalDate.of(2004, 11, 22));
+        assertThat(secondPractitioner.getCeasedToActOn()).isEqualTo(LocalDate.of(2006, 5, 5));
+        assertThat(secondPractitioner.getRole()).isEqualTo(Practitioners.RoleEnum.PRACTITIONER);
+
+        PractitionerAddress secondAddress = secondPractitioner.getAddress();
+        assertThat(secondAddress.getAddressLine1()).isEqualTo("Free From Debt");
+        assertThat(secondAddress.getAddressLine2()).isEqualTo("101 Barbirolli Square");
+        assertThat(secondAddress.getLocality()).isEqualTo("Lower Mosley Street");
+        assertThat(secondAddress.getRegion()).isEqualTo("Liverpool");
+        assertThat(secondAddress.getCountry()).isNull();
+        assertThat(secondAddress.getPostalCode()).isEqualTo("L2 4PO");
+
+        assertThat(firstCase.getDates().size()).isEqualTo(1);
         List<CaseDates> caseDates = firstCase.getDates();
-        assertThat(caseDates.get(0).getType()).isEqualTo(CaseDates.TypeEnum.WOUND_UP_ON);
-        assertThat(caseDates.get(0).getDate()).isEqualTo(LocalDate.of(2020, 05, 06));
-        assertThat(caseDates.get(1).getType()).isEqualTo(CaseDates.TypeEnum.DECLARATION_SOLVENT_ON);
-        assertThat(caseDates.get(1).getDate()).isEqualTo(LocalDate.of(2020, 04, 29));
+        assertThat(caseDates.get(0).getType()).isEqualTo(CaseDates.TypeEnum.INSTRUMENTED_ON);
+        assertThat(caseDates.get(0).getDate()).isEqualTo(LocalDate.of(2002, 12, 17));
 
         assertThat(firstCase.getLinks()).isNotNull();
         String mortgageId = insolvency.getCaseNumbers().get(0).getMortgageId().toString();
         String expectedChargeLink = "/company/" + companyNumber + "/charges/" + mortgageId;
         assertThat(firstCase.getLinks().getCharge()).isEqualTo(expectedChargeLink);
+
+        // SECOND CASE
+        ModelCase secondCase = insolvencyTarget.getCases().get(1);
+        assertThat(secondCase.getNumber()).isEqualTo(2);
+        assertThat(secondCase.getType()).isEqualTo(ModelCase.TypeEnum.CREDITORS_VOLUNTARY_LIQUIDATION);
+        assertThat(secondCase.getPractitioners().size()).isEqualTo(1);
+
+        Practitioners secondCasePractitioner = secondCase.getPractitioners().get(0);
+        assertThat(secondCasePractitioner.getName()).isEqualTo("Mark Terence Getliffe");
+        assertThat(secondCasePractitioner.getAppointedOn()).isEqualTo(LocalDate.of(2005, 9, 29));
+        assertThat(secondCasePractitioner.getRole()).isEqualTo(Practitioners.RoleEnum.RECEIVER);
+
+        PractitionerAddress secondCaseAddress = secondCasePractitioner.getAddress();
+        assertThat(secondCaseAddress.getAddressLine1()).isEqualTo("78 Gluten Tag Road");
+        assertThat(secondCaseAddress.getAddressLine2()).isNull();
+        assertThat(secondCaseAddress.getLocality()).isEqualTo("Manchester");
+        assertThat(secondCaseAddress.getRegion()).isNull();
+        assertThat(secondCaseAddress.getCountry()).isNull();
+        assertThat(secondCaseAddress.getPostalCode()).isEqualTo("M2 4WU");
+
+        assertThat(secondCase.getDates().size()).isEqualTo(2);
+        List<CaseDates> secondCaseDates = secondCase.getDates();
+        assertThat(secondCaseDates.get(0).getType()).isEqualTo(CaseDates.TypeEnum.WOUND_UP_ON);
+        assertThat(secondCaseDates.get(0).getDate()).isEqualTo(LocalDate.of(2005, 9, 29));
+        assertThat(secondCaseDates.get(1).getType()).isEqualTo(CaseDates.TypeEnum.DISSOLVED_ON);
+        assertThat(secondCaseDates.get(1).getDate()).isEqualTo(LocalDate.of(2021, 10, 01));
+
+        assertThat(secondCase.getLinks()).isNull();
     }
 }
