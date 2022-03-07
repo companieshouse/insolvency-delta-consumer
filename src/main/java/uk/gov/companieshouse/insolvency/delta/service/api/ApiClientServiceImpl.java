f99968ae402c45aacc99d319e4c8e62463d0cf76
Dominic Stevenson <dstevenson@companieshouse.gov.uk>
2022-03-07 17:20:36 +0000

    Added API key for insolvency-data-api

--- src/main/java/uk/gov/companieshouse/insolvency/delta/service/api/ApiClientServiceImpl.java
+++ src/main/java/uk/gov/companieshouse/insolvency/delta/service/api/ApiClientServiceImpl.java
@@ -20,7 +20,7 @@ import uk.gov.companieshouse.logging.Logger;
 @Service
 public class ApiClientServiceImpl extends BaseApiClientServiceImpl implements ApiClientService {
 
-    @Value("${api.internal-api-key}")
+    @Value("${api.insolvency-data-api-key}")
     private String chsApiKey;
 
     @Value("${api.api-url}")
