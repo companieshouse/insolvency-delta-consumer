5e2f6bce5a3f58655dc170fa7a866b85e6e541f1
Ahmed <a.mobarak92@gmail.com>
2022-03-29 12:12:54 +0100

    Commented out tests

--- src/test/java/uk/gov/companieshouse/insolvency/delta/serialization/ChsDeltaDeserializerTest.java
+++ src/test/java/uk/gov/companieshouse/insolvency/delta/serialization/ChsDeltaDeserializerTest.java
@@ -1,27 +1,27 @@
-package uk.gov.companieshouse.insolvency.delta.serialization;
-
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
-import uk.gov.companieshouse.delta.ChsDelta;
-import uk.gov.companieshouse.logging.Logger;
-
-import static org.assertj.core.api.Assertions.assertThat;
-
-@ExtendWith(MockitoExtension.class)
-public class ChsDeltaDeserializerTest {
-
-    @Mock
-    private Logger logger;
-    private ChsDeltaDeserializer deserializer;
-
-    @BeforeEach
-    public void init() {
-        deserializer = new ChsDeltaDeserializer(logger);
-    }
-
+//package uk.gov.companieshouse.insolvency.delta.serialization;
+//
+//import org.junit.jupiter.api.BeforeEach;
+//import org.junit.jupiter.api.Test;
+//import org.junit.jupiter.api.extension.ExtendWith;
+//import org.mockito.Mock;
+//import org.mockito.junit.jupiter.MockitoExtension;
+//import uk.gov.companieshouse.delta.ChsDelta;
+//import uk.gov.companieshouse.logging.Logger;
+//
+//import static org.assertj.core.api.Assertions.assertThat;
+//
+//@ExtendWith(MockitoExtension.class)
+//public class ChsDeltaDeserializerTest {
+//
+//    @Mock
+//    private Logger logger;
+//    private ChsDeltaDeserializer deserializer;
+//
+//    @BeforeEach
+//    public void init() {
+//        deserializer = new ChsDeltaDeserializer(logger);
+//    }
+//
 //    @Test
 //    void When_deserialize_Expect_ValidChsDeltaObject() {
 //        ChsDelta chsDelta = new ChsDelta("{\"key\": \"value\"}", 1, "context_id");
@@ -31,9 +31,9 @@ public class ChsDeltaDeserializerTest {
 //
 //        assertThat(deserializedObject).isEqualTo(chsDelta);
 //    }
-
-    private byte[] encodedData(ChsDelta chsDelta) {
-        ChsDeltaSerializer serializer = new ChsDeltaSerializer(null);
-        return serializer.serialize("", chsDelta);
-    }
-}
+//
+//    private byte[] encodedData(ChsDelta chsDelta) {
+//        ChsDeltaSerializer serializer = new ChsDeltaSerializer(null);
+//        return serializer.serialize("", chsDelta);
+//    }
+//}
