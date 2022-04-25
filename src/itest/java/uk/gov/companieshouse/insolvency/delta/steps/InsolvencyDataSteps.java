353a800e27c197ccd8662b15f470c83b408ca549
vchitti-kainos <98651276+vchitti-kainos@users.noreply.github.com>
2022-04-25 14:56:56 +0100

    DSND-699: Added integration tests (#36)
    Co-authored-by: Vamshidhar Chitti <vchitti@pd12634.companieshouse.local>

--- src/itest/java/uk/gov/companieshouse/insolvency/delta/steps/InsolvencyDataSteps.java
+++ src/itest/java/uk/gov/companieshouse/insolvency/delta/steps/InsolvencyDataSteps.java
@@ -11,13 +11,18 @@ import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.apache.commons.io.FileUtils;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.test.utils.KafkaTestUtils;
 import org.springframework.util.ResourceUtils;
 import uk.gov.companieshouse.delta.ChsDelta;
+import uk.gov.companieshouse.stream.ResourceChangedData;
 
 import static com.github.tomakehurst.wiremock.client.WireMock.*;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -28,11 +33,17 @@ public class InsolvencyDataSteps {
 
     private String companyNumber;
 
+    @Value("${insolvency.delta.topic}")
+    private String topic;
+
     @Autowired
     public KafkaTemplate<String, Object> kafkaTemplate;
 
     @Autowired
     protected TestRestTemplate restTemplate;
+
+    @Autowired
+    public KafkaConsumer<String, Object> kafkaConsumer;
 
     @Given("Insolvency delta consumer service is running")
     public void insolvency_delta_consumer_service_is_running() {
@@ -44,13 +55,70 @@ public class InsolvencyDataSteps {
     @When("a {string} with {string} is published to the topic {string} and consumed")
     public void a_with_is_published_to_the_topic_and_consumed(String message, String companyNumber, String topicName)
             throws InterruptedException {
+        startWiremockServer();
         this.companyNumber = companyNumber;
 
-        stubInsolvencyDataApiServiceCalls(companyNumber);
+        stubInsolvencyDataApiServiceCalls(companyNumber, 200);
         kafkaTemplate.send(topicName, createMessage(message));
 
         CountDownLatch countDownLatch = new CountDownLatch(1);
         countDownLatch.await(5, TimeUnit.SECONDS);
+    }
+
+    @When("a non-avro message is published and failed to process")
+    public void a_non_avro_message_is_published_and_failed_to_process() throws InterruptedException {
+        startWiremockServer();
+        kafkaTemplate.send(topic, "invalid message");
+
+        CountDownLatch countDownLatch = new CountDownLatch(1);
+        countDownLatch.await(5, TimeUnit.SECONDS);
+    }
+
+    @When("a valid message is published with invalid json")
+    public void a_valid_message_is_published_with_invalid_json() throws InterruptedException {
+        startWiremockServer();
+        String invalidJson = loadFile("classpath:input/case_type_invalid.json");
+        ChsDelta chsDelta = ChsDelta.newBuilder()
+                .setData(invalidJson)
+                .setContextId("context_id")
+                .setAttempt(1)
+                .build();
+        kafkaTemplate.send(topic, chsDelta);
+
+        CountDownLatch countDownLatch = new CountDownLatch(1);
+        countDownLatch.await(5, TimeUnit.SECONDS);
+    }
+
+    @When("a message {string} is published for {string} and stubbed insolvency-data-api returns {string}")
+    public void a_message_is_published_for_and_stubbed_insolvency_data_api_returns(
+            String message, String companyNumber, String statusCode) throws InterruptedException {
+        startWiremockServer();
+        this.companyNumber = companyNumber;
+
+        stubInsolvencyDataApiServiceCalls(companyNumber, Integer.parseInt(statusCode));
+        kafkaTemplate.send(topic, createMessage(message));
+
+        CountDownLatch countDownLatch = new CountDownLatch(1);
+        countDownLatch.await(5, TimeUnit.SECONDS);
+    }
+
+    @When("a message {string} is published for {string} with unexpected data")
+    public void a_message_is_published_for_with_unexpected_data(String message, String companyNumber) throws InterruptedException {
+        startWiremockServer();
+        this.companyNumber = companyNumber;
+        kafkaTemplate.send(topic, createMessage(message));
+
+        CountDownLatch countDownLatch = new CountDownLatch(1);
+        countDownLatch.await(5, TimeUnit.SECONDS);
+    }
+
+    @Then("the message should be moved to topic {string}")
+    public void the_message_should_be_moved_to_topic(String topic) {
+        ConsumerRecord<String, Object> singleRecord = KafkaTestUtils.getSingleRecord(kafkaConsumer, topic);
+
+        assertThat(singleRecord.value()).isNotNull();
+
+        wireMockServer.stop();
     }
 
     @Then("verify PUT method is called on insolvency-data-api service with body {string}")
@@ -68,15 +136,18 @@ public class InsolvencyDataSteps {
         wireMockServer.stop();
     }
 
-    private void stubInsolvencyDataApiServiceCalls(String chNumber) {
-        wireMockServer = new WireMockServer(8888);
-        wireMockServer.start();
-        configureFor("localhost", 8888);
+    private void stubInsolvencyDataApiServiceCalls(String chNumber, int statusCode) {
         stubFor(
                 put(urlPathEqualTo("/company/"+chNumber+"/insolvency"))
                         .withRequestBody(containing(chNumber))
                         .willReturn(aResponse()
-                                .withStatus(200)));
+                                .withStatus(statusCode)));
+    }
+
+    private void startWiremockServer() {
+        wireMockServer = new WireMockServer(8888);
+        wireMockServer.start();
+        configureFor("localhost", 8888);
     }
 
     private String loadFile(String fileName) {
