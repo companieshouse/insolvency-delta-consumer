bb1593d34786e51daa77d4ff593486237b646abe
Zahid Muhammad <97745759+zahidm3@users.noreply.github.com>
2022-06-14 12:33:52 +0100

    Increasing timeout to 2sec (#61)
    * Increasing timeout to 2sec
    
    * Log improvement

--- src/main/java/uk/gov/companieshouse/insolvency/delta/consumer/InsolvencyDeltaConsumer.java
+++ src/main/java/uk/gov/companieshouse/insolvency/delta/consumer/InsolvencyDeltaConsumer.java
@@ -79,7 +79,8 @@ public class InsolvencyDeltaConsumer {
             }
         } catch (Exception exception) {
             logger.error(format("Exception occurred while processing the topic: %s "
-                    + "with contextId: %s", topic, contextId), exception);
+                            + "with contextId: %s, exception thrown: %s",
+                    topic, contextId, exception), exception);
             throw exception;
         }
     }
