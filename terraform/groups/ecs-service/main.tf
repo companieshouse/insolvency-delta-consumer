e8c37f570dedb83410a4ee536f98f629e9309e04
scunnahhyland-ch <scunnahhyland@companieshouse.gov.uk>
2025-07-04 12:01:27 +0100

    Updated terraform tag for kafka error

--- terraform/groups/ecs-service/main.tf
+++ terraform/groups/ecs-service/main.tf
@@ -77,7 +77,7 @@ module "ecs-service" {
 }
 
 module "ecs-service-kafka-error" {
-  source = "git@github.com:companieshouse/terraform-modules//aws/ecs/ecs-service?ref=1.0.324"
+  source = "git@github.com:companieshouse/terraform-modules//aws/ecs/ecs-service?ref=1.0.334"
 
   # Environmental configuration
   environment                    = var.environment
